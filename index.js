d970f7d468fbf6b4497a3c16632455fbbe7d6f52
Nimrashoaib010 <shoaibnim010@gmail.com>
2024-10-02 23:58:36 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,70 @@
+var person = {
+    firstName: "Nimra",
+    lastName: "Shoaib",
+    age: 18,
+    qualification: "Inter",
+};
+
+document.getElementById("prsnl").innerHTML =
+person.firstName + person.lastName + "is" +
+person.age + "years old." + "and I have completed the" +
+person.qualification + "in 2023.";
+
+var business = {
+    name: "Basics",
+    price: 3.99,
+    transfer: 1000,
+    pages: 10,
+    space: 120,
+};
+
+var Car = {
+    name: "City",
+    model: 2020,
+    color: "Black",
+    price: 2000000,
+};
+
+var Bike = {
+    name: "yamaha",
+    model: 2015,
+    color: "yellow",
+    price: 100000,
+};
+
+var Mango = {
+    color: "yellow",
+    taste: "Sweet",
+    types: "different"
+};
+
+var reportCard = {
+    totalMarks: 100,
+    maxMarks: 45,
+    obtMarks: 90,
+};
+
+var tablet ={
+    name: "Ipad",
+    color: "White",
+    price: 80000,
+};
+
+var watches = {
+    name: "Rolex",
+    color: "silver",
+    price: 20000,
+};
+
+var dress ={
+    outlet: "Beechtree",
+    type: "Khadar",
+    price: 5000,
+};
+
+var student ={
+    name: "John",
+    Position: "First",
+    grade: "A+",
+    percentage: 91,
+};
